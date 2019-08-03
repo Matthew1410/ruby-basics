22e5ff15818ac58058660b89db82f38a7c3bfcfb
Matthew Montiveros <matthewmontiveros@gmail.com>
2019-08-03 19:45:46 +0800

    Adds practice2

---
+++ practice2.rb
@@ -0,0 +1,6 @@
+puts "Enter the first number:"
+first_number = gets.chomp.to_i
+puts "Enter the second number:"
+second_number = gets.chomp.to_i
+sum = first_number + second_number
+puts "The sum of the numbers is:" + sum.to_s
