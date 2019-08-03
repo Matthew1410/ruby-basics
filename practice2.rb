a38e29971008d24abde2021b7e0b74d77b6368c7
Matthew Montiveros <matthewmontiveros@gmail.com>
2019-08-03 19:54:47 +0800

    Adds greater than or Less than

--- practice2.rb
+++ practice2.rb
@@ -4,3 +4,9 @@ puts "Enter the second number:"
 second_number = gets.chomp.to_i
 sum = first_number + second_number
 puts "The sum of the numbers is:" + sum.to_s
+
+if sum > 100
+    puts "The sum is greater than 100"
+else
+    puts "The sum is less than 100"
+end
