fed043b9cccdd2f8e7c75c7d70a939c0a6e9af4d
Matthew Montiveros <matthewmontiveros@gmail.com>
2019-08-06 14:29:12 +0800

    String Interpolation

--- practice2.rb
+++ practice2.rb
@@ -1,13 +1,13 @@
-puts "Enter the first number:"
+print "Enter the first number:"
 first_number = gets.chomp.to_i
-puts "Enter the second number:"
+print "Enter the second number:"
 second_number = gets.chomp.to_i
-sum = first_number + second_number
-puts "The sum of the numbers is:" + sum.to_s
 
-if sum > 100
+puts "The sum of the numbers is: #{first_number + second_number}"
+
+if first_number + second_number > 100
     puts "The sum is greater than 100"
-elsif sum < 100
+elsif first_number + second_number < 100
     puts "The sum is less than 100"
 else
     puts "The sum is equal to 100"
