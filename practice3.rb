e3662ce8334769bc5ddfa06597c5143b9d5538dc
Matthew Montiveros <matthewmontiveros@gmail.com>
2019-08-10 17:52:20 +0800

    Reverse and Sort using refractor

--- practice3.rb
+++ practice3.rb
@@ -2,6 +2,10 @@ def sort_name(name)
   name.downcase.chars.sort.join.capitalize
 end
 
+def reverse_name(name)
+  name.reverse.capitalize
+end
+
 print "Please enter your name: "
 name = gets.chomp
 print "Type UP for uppercase and DOWN for lowercase: "
@@ -16,5 +20,5 @@ else
 end
 
 puts "There are #{name.length} characters in your name"
-puts "Reversed, your name is #{name.reverse.capitalize}"
+puts "Reversed, your name is #{reverse_name(name)} "
 puts "Sorted by letter, your name is #{sort_name(name)} "
