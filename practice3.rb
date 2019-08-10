856702a30c5800ebef75ec97feefbcb1ae49b490
Matthew Montiveros <matthewmontiveros@gmail.com>
2019-08-10 18:26:54 +0800

    Check name if contains entered letters

--- practice3.rb
+++ practice3.rb
@@ -22,3 +22,12 @@ end
 puts "There are #{name.length} characters in your name"
 puts "Reversed, your name is #{reverse_name(name)} "
 puts "Sorted by letter, your name is #{sort_name(name)} "
+
+print "Enter some letters: "
+some_letters = gets.chomp
+
+if name.include? some_letters
+  puts "Your name contains #{some_letters}"
+else
+  puts "It doesn't contains in your Name."
+end
