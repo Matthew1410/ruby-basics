816978fd2b504f2b5c84e94dc02d80e6b567c3dd
Matthew Montiveros <matthewmontiveros@gmail.com>
2019-08-10 16:53:10 +0800

    Name length

--- practice3.rb
+++ practice3.rb
@@ -1,12 +1,14 @@
 print "Please enter your name: "
 name = gets.chomp
-print "Type UP for uppercase and DOWN for lowercase:"
-option = gets.chomp
+print "Type UP for uppercase and DOWN for lowercase: "
+option = gets.chomp.downcase
 
-if option == "UP"
+if option == "up"
   puts name.upcase
-elsif option == "DOWN"
+elsif option == "down"
   puts name.downcase
 else
   puts "Wrong input!"
 end
+
+puts "There are #{name.length} characters in your name"
