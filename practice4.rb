39d9d135b9120a27e4fad1471539997f50a60e02
Matthew Montiveros <matthewmontiveros@gmail.com>
2019-08-12 13:52:25 +0800

    Added If-else statement in conversion from number to roman numerals

--- practice4.rb
+++ practice4.rb
@@ -1,5 +1,14 @@
+
 print "Enter a number: "
 num = gets.to_i
+
+one_thousand = "M"
+five_hundred = "D"
+one_hundred = "C"
+fifty = "L"
+ten = "X"
+five = "V"
+one = "I"
 
 if num > 1000
   thousands = num / 1000
@@ -10,5 +19,24 @@ if num > 1000
   num %= 10
   ones = num / 1
 
-  puts thousands.to_s + hundreds.to_s + tens.to_s + ones.to_s
+elsif num > 100 && num < 1000
+
+  hundreds = num / 100
+  num %= 100
+  tens = num / 10
+  num %= 10
+  ones = num / 1
+
+elsif num > 10 && num < 100
+
+  tens = num / 10
+  num %= 10
+  ones = num / 1
+
+elsif num > 1 && num < 10
+  ones = num / 1
+ 
+else
+  puts "You input an invalid number."
 end
+
