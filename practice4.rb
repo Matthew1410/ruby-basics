1df9bcf9540a8c46cd220d2529c330d43f7ad6be
Matthew Montiveros <matthewmontiveros@gmail.com>
2019-08-17 16:22:21 +0800

    Refactors code

--- practice4.rb
+++ practice4.rb
@@ -12,58 +12,41 @@ roman_thousand = "M" * thousands
 
 num %= 1000
 
-puts roman_thousand, num
-# hundreds = num / 100
-# if hundreds >= 1 && hundreds < 4
-#   roman_hundreds = "C" * hundreds
-# elsif hundreds == 4
-#   roman_hundreds = "CD"
-# elsif hundreds == 5
-#   roman_hundreds = "D"
-# elsif hundreds >= 6 && hundreds < 9
-#   hundreds -= 5
-#   six_hundred = "C" * hundreds
-#   roman_hundreds = "D" + six_hundred
-# elsif hundreds == 9
-#   roman_hundreds = "CM"
-# end
+hundreds = num / 100
+roman_hundreds = case hundreds
+when 0..3 then "C" * hundreds
+when 4 then "CD"
+when 5 then "D"
+when 6 then "DC"
+when 7 then "DCC"
+when 8 then "DCCC"
+when 9 then "CM"
+end
 
-# num %= 100
+num %= 100
 
-# #tens
+tens = num / 10
+roman_tens = case tens
+when 0..3 then "X" * tens
+when 4 then "XL"
+when 5 then "L"
+when 6 then "XL"
+when 7 then "XLL"
+when 8 then "XLLL"
+when 9 then "XC"
+end
 
-# tens = num / 10
-# if tens >= 1 && tens < 4
-#   roman_tens = "X" * tens
-# elsif tens == 4
-#   roman_tens = "XL"
-# elsif tens == 5
-#   roman_tens = "L"
-# elsif tens >= 6 && tens < 9
-#   tens -= 5
-#   six_tens = "X" * tens
-#   roman_tens = "L" + six_tens
-# elsif tens == 9
-#   roman_tens = "XC"
-# end
+num %=10
 
-# num %=10
+ones = num
+roman_ones = case ones
+when 0..3 then "I" * ones
+when 4 then "IV"
+when 5 then "V"
+when 6 then "VI"
+when 7 then "VII"
+when 8 then "VIII"
+when 9 then "IX"
+end
 
-# #ones
-
-# ones = num
-# if ones >= 1 && ones < 4
-#   roman_ones = "I" * ones
-# elsif ones == 4
-#   roman_ones = "IV"
-# elsif ones == 5
-#   roman_ones = "V"
-# elsif ones >= 6 && ones < 9
-#   ones -= 5
-#   six_ones = "I" * ones
-#   roman_ones = "V" + six_ones
-# elsif ones == 9
-#   roman_hundreds = "IX"
-# end
-
-# puts "Converted to: #{roman_thousand.to_s + roman_hundreds.to_s + roman_tens.to_s + roman_ones.to_s}"
+puts "Converted to: #{roman_thousand.to_s + roman_hundreds.to_s + roman_tens.to_s + roman_ones.to_s}"
