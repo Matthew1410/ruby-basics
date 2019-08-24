b94a7fe2012b38c83e81d8e16a963bdb0400889b
Jem Pimentel <jem.pimentel@outlook.com>
2019-08-24 18:11:30 +0800

    Includes validation for minutes; fixes formatting

--- practice5.rb
+++ practice5.rb
@@ -3,18 +3,20 @@ def military_to_12_hour_time(military_time)
 
   hours = military_time / 100
   minutes = military_time % 100
+  raise ArgumentError if minutes >= 60
+
   period = (hours / 12).zero? ? "AM" : "PM"
   hours = case hours
           when 0, 12 then 12
           when 1..11 then hours
           when 12..23 then hours % 12
           end
-  "#{hours}:#{minutes} #{period}"
+  format("%02d:%02d %s", hours, minutes, period)
 end
 
 print "24 hour format time: "
 begin
-  normal_time = military_to_12_hour_time(Integer(gets))
+  normal_time = military_to_12_hour_time(Integer(gets, 10))
   puts "The 12 hour format is: #{normal_time}"
 rescue ArgumentError
   puts "The time you entered is invalid."
