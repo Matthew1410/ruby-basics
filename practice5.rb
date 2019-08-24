7b54ef9d4ff1d2c581411b24c28c4cb3834b5b07
Jem Pimentel <jem.pimentel@outlook.com>
2019-08-24 16:39:51 +0800

    Adds period logic and stricter validation

--- practice5.rb
+++ practice5.rb
@@ -1,18 +1,20 @@
+
 def military_to_12_hour_time(military_time)
   return if military_time >= 2400 && military_time.negative?
 
   hours = military_time / 100
   minutes = military_time % 100
+  period = (hours / 12).zero? ? "AM" : "PM"
   hours = case hours
           when 0, 12 then 12
           when 1..11 then hours
           when 12..23 then hours % 12
           end
-  "#{hours}:#{minutes}"
+  "#{hours}:#{minutes} #{period}"
 end
 
 print "24 hour format time: "
-normal_time = military_to_12_hour_time(gets.to_i)
+normal_time = military_to_12_hour_time(Integer(gets))
 if normal_time
   puts "The 12 hour format is: #{normal_time}"
 else
