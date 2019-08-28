1e146646e3a5231093b56a7fda029aa71718deda
Matthew Montiveros <matthewmontiveros@gmail.com>
2019-08-29 01:56:20 +0800

    Dice game WIP

--- practice6.rb
+++ practice6.rb
@@ -1,9 +1,19 @@
 class Dice
-  def initialize(number_of_sides)
-    # code here
+  def initial(number_of_sides)
+    sides = number_of_sides
   end
 
   def roll(number_of_rolls)
-    # code here
+    times_rolled = number_of_rolls
+    rand(number_of_rolls) + 1
   end
 end
+
+print "Choose number of sides (6, 8, 10, 20): "
+  number_sides = initial(Integer(gets.to_i))
+
+print "Enter number of rolls: "
+    number_rolls = roll(gets.to_i)
+
+puts "You picked #{number_sides} sides!"
+puts "You rolled #{number_rolls} times!"
