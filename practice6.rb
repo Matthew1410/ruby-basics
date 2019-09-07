fccef821fa7b147dc37b503e4277edc15c7a5743
Jem Pimentel <jem.pimentel@outlook.com>
2019-09-07 15:57:57 +0800

    Adds variable number of dice to roll method

--- practice6.rb
+++ practice6.rb
@@ -5,7 +5,11 @@ class Dice
     @number_of_sides = number_of_sides
   end
 
-  def roll
-    rand(1..@number_of_sides)
+  def roll(number_of_rolls=1)
+    total_roll = 0
+    number_of_rolls.times do
+      total_roll += rand(1..@number_of_sides)
+    end
+    total_roll
   end
 end
