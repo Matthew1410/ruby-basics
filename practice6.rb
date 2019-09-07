1d3c55ef0aa6c0bed102887c68288454a8815dd4
Jem Pimentel <jem.pimentel@outlook.com>
2019-09-07 16:16:21 +0800

    Adds exception handling

--- practice6.rb
+++ practice6.rb
@@ -2,10 +2,14 @@ class Dice
   attr_accessor :number_of_sides
 
   def initialize(number_of_sides)
+    raise ArgumentError unless number_of_sides > 2 && number_of_sides.is_a?(Integer)
+
     @number_of_sides = number_of_sides
   end
 
   def roll(number_of_rolls=1)
+    raise ArgumentError if number_of_rolls.negative?
+
     total_roll = 0
     number_of_rolls.times do
       total_roll += rand(1..@number_of_sides)
