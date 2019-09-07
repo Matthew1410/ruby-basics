300cc830f3812ef35c2a8e6776dc2b2737b93fc4
Jem Pimentel <jem.pimentel@outlook.com>
2019-09-07 15:27:10 +0800

    Class with accessor methods

--- practice6.rb
+++ practice6.rb
@@ -1,37 +1,19 @@
 class Dice
-  def initial(num_of_sides)
-    @sides = num_of_sides
+  @number_of_sides = 0
+  @color = ""
+
+  def initialize(number_of_sides)
+    @number_of_sides = number_of_sides
   end
 
-  def roll(num_of_rolls)
-    @rolls = num_of_rolls
+  def number_of_sides
+    @number_of_sides
   end
 
-  def generate_dice_roll
-    rand(@sides) + 1
+  def number_of_sides=(number_of_sides)
+    @number_of_sides = number_of_sides
   end
 
-  def play(num_of_rolls)
-    roll_array = []
-    num_of_rolls.times do
-      roll_array << generate_dice_roll
-    end
-    total = 1
-    roll_array.each do |roll|
-      new_total = total + roll
-      total = new_total
-    end
-    total
+  def roll
   end
 end
-
-print "Enter number of sides: "
-num_of_sides = gets.to_i
-print "Enter number of rolls: "
-num_of_rolls = gets.to_i
-
-output = Dice.new
-
-puts "The number of sides is #{output.initial(num_of_sides)}"
-puts "The number of rolls is #{output.roll(num_of_rolls)}"
-print "You rolled #{output.play(num_of_rolls)} using ", output.roll(num_of_rolls), " ", output.initial(num_of_sides), "-sided dice"
