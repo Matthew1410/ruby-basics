08b5c69795baa693d1d28f695ee52b18a25c2456
Jem Pimentel <jem.pimentel@outlook.com>
2019-09-07 15:30:06 +0800

    Class with attr_reader and attr_writer

--- practice6.rb
+++ practice6.rb
@@ -1,16 +1,10 @@
 class Dice
-  @number_of_sides = 0
+  attr_reader :number_of_sides
+  attr_writer :number_of_sides
+
   @color = ""
 
   def initialize(number_of_sides)
-    @number_of_sides = number_of_sides
-  end
-
-  def number_of_sides
-    @number_of_sides
-  end
-
-  def number_of_sides=(number_of_sides)
     @number_of_sides = number_of_sides
   end
 
