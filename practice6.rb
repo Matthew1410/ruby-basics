0a755430e29f21fb5c249e8a4664047950eeaa83
Jem Pimentel <jem.pimentel@outlook.com>
2019-09-07 15:35:21 +0800

    Class with attr_accessor

--- practice6.rb
+++ practice6.rb
@@ -1,6 +1,8 @@
 class Dice
-  attr_reader :number_of_sides
-  attr_writer :number_of_sides
+  # attr_reader :number_of_sides
+  # attr_writer :number_of_sides
+
+  attr_accessor :number_of_sides
 
   @color = ""
 
