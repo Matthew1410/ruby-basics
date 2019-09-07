9a490e14e2b19d1601b8f78e9bd3a226ceee9583
Jem Pimentel <jem.pimentel@outlook.com>
2019-09-07 15:44:25 +0800

    Adds roll method

--- practice6.rb
+++ practice6.rb
@@ -1,15 +1,11 @@
 class Dice
-  # attr_reader :number_of_sides
-  # attr_writer :number_of_sides
-
   attr_accessor :number_of_sides
-
-  @color = ""
 
   def initialize(number_of_sides)
     @number_of_sides = number_of_sides
   end
 
   def roll
+    rand(1..@number_of_sides)
   end
 end
